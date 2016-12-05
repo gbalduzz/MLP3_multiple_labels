51f5ab0a921a55463df91701d15688f3e206c14a
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-12-05 13:19:40 +0100

    updated CV

--- src/forestCV.py
+++ src/forestCV.py
@@ -1,12 +1,10 @@
-from modules import  preprocessing
 import numpy as np
 from sklearn import preprocessing
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import *
+from sklearn.multioutput import MultiOutputClassifier
 from sklearn import metrics, model_selection
-from modules import postprocess
 import h5py
 import time
-from  modules.kernels import min_histo
 
 print("startup")
 
@@ -24,8 +22,9 @@ train = scaler.transform(train)
 
 #get the scores
 print("computing the score")
-regr = RandomForestClassifier(criterion='gini', n_jobs=1, n_estimators=1000)
-scorer = metrics.make_scorer(metrics.log_loss, greater_is_better=False, needs_proba=True)
+forest = RandomForestClassifier(criterion='entropy', n_jobs=1, n_estimators=1000)
+regr = MultiOutputClassifier(forest, n_jobs=1)
+scorer = metrics.make_scorer(metrics.hamming_loss, greater_is_better=False, needs_proba=False)
 scores = model_selection.cross_val_score(regr, train, y, scoring=scorer, cv=5, n_jobs=-1)
 avg = np.average(scores)
 
